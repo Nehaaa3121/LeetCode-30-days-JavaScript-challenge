31ac67d9521b377f7a0886ec5c4be220e2f2b6ac
Nehaa3121 <nehachoudhary3198@gmail.com>
2023-05-05 13:54:49 +0530

    Day 1

---
+++ Problem of the day 1/helloworld.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    {
+        return f=(...args)=> "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
